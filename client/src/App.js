0d5889f4d2e57649636d75da48ed09dad697e083
Cman131 <conor13@msn.com>
2024-05-02 00:19:32 -0400

    styled up the webpages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link, useLocation } from "react-router-dom";
 import './App.css';
 
 export default function App() {
@@ -8,6 +8,8 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="events" element={<Events />} />
+          <Route path="resources" element={<Resources />} />
+          <Route path="tournaments" element={<Tournaments />} />
 
           <Route path="*" element={<NoMatch />} />
         </Route>
@@ -17,21 +19,34 @@ export default function App() {
 }
 
 function Layout() {
+  const { pathname } = useLocation();
   return (
     <div>
       <nav>
         <ul>
-          <li>
+          <li class={pathname === "/" ? "current" : ""}>
             <Link to="/">
               <img alt="CHS Riichi Club Logo" src="./logo_alpha.png" />
               <h1>Charleston Riichi Mahjong</h1>
             </Link>
           </li>
-          <li>
+          <li class={pathname === "/events" ? "current" : ""}>
             <Link to="/events">Events</Link>
+          </li>
+          <li class={pathname === "/tournaments" ? "current" : ""}>
+            <Link to="/tournaments">Tournaments</Link>
+          </li>
+          <li class={pathname === "/resources" ? "current" : ""}>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>
+            <a rel="noreferrer" target="_blank" href="https://shop.printyourcause.com/campaigns/charleston-riichi-mahjong-club">Merch Shop</a>
           </li>
           <li>
             <a rel="noreferrer" target="_blank" href="https://www.meetup.com/charleston-riichi-mahjong/">Meetup</a>
+          </li>
+          <li>
+            <a rel="noreferrer" target="_blank" href="https://discord.gg/xhZtZZF3Jk">Discord Server</a>
           </li>
         </ul>
       </nav>
@@ -59,6 +74,66 @@ function Events() {
   return (
     <div class="event-pane">
       <iframe src='https://widgets.sociablekit.com/meetup-group-events/iframe/25406359' frameborder='0' width='100%' height='100%' title="Upcoming events"></iframe>
+    </div>
+  );
+}
+
+function Resources() {
+  return (
+    <div>
+      <div class="link-list">
+        <h2>Learning Resources</h2>
+        <ul>
+          <li><a rel="noreferrer" target="_blank" href="https://docs.google.com/document/d/1vtuRiT5a6QBx7ggaOQ5P4SlnySxssZxbX6uZeHKYzOI/edit?usp=sharing">Yaku Reference Book</a></li>
+          <li><a rel="noreferrer" target="_blank" href="https://docs.google.com/document/d/1Cai2O3TsZyv3nV-gXU46I6taYoDFjlutNiOxS2n-BB0/edit?usp=sharing">How to score your hand</a></li>
+          <li><a rel="noreferrer" target="_blank" href="https://docs.google.com/document/d/1oiVkcSWLIm0ZESQtXItH0EoWTAexqvhl7mrHcRQZ--M/edit?usp=sharing">Scoring quiz</a>: To practice with. Answers in comments.</li>
+          <li><a rel="noreferrer" target="_blank" href="https://ooyamaneko.net/download/mahjong/riichi/Daina_Chiba_-_Riichi_Book_1_en.pdf">Riichi book</a>: A highly recommended strategy reference written in English</li>
+        </ul>
+      </div>
+      <div class="link-list">
+        <h2>Recommended Applications</h2>
+        <ul>
+          <li><a rel="noreferrer" target="_blank" href="https://mahjongsoul.game.yo-star.com/">Mahjong Soul</a>: A phenomenal application to play riichi mahjong online for Mobile and Browser. Can play AI, friends, and/or online people.</li>
+          <li><a rel="noreferrer" target="_blank" href="https://play.google.com/store/apps/details?id=ric.ov.RiichiCalc">Riichi Calc</a>: A scoring app that's really good for Android.</li>
+          <li><a rel="noreferrer" target="_blank" href="https://apps.apple.com/us/app/riichi-mahjong-hand-calculator/id1160349726">Riichi Mahjong Hand Calculator</a>: A scoring app that's okay and for Apple products.</li>
+          <li><a rel="noreferrer" target="_blank" href="https://tenhou.net/4/">Tenhou</a>: An application to play riichi mahjong online like Mahjong soul. This one is more popular with hipsters.</li>
+        </ul>
+      </div>
+      <div class="link-list">
+        <h2>Recommended Products</h2>
+        <ul>
+          <li><a rel="noreferrer" target="_blank" href="https://www.amazon.com/dp/B003UU129U/ref=emc_b_5_t">Regular Riichi Tileset</a></li>
+          <li><a rel="noreferrer" target="_blank" href="https://www.amazon.com/gp/product/B003UTX4L0/ref=ppx_yo_dt_b_search_asin_title">Conor's Black/Yellow Riichi Tileset</a></li>
+          <li><a rel="noreferrer" target="_blank" href="https://www.amazon.com/gp/product/B0017KHW3A/ref=ppx_yo_dt_b_asin_title_o00_s00">Junkmat</a>: To play on.</li>
+          <li><a rel="noreferrer" target="_blank" href="https://www.amazon.co.jp/-/en/AMOS-COMPASS-Mahjong-Support-Plate/dp/B085T6ZL47/ref=sr_1_2">Amos Compass</a>: To indicate seats and place tiles against.</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Tournaments() {
+  if (Date.now() > new Date(2024, 27, 27)) {
+    return (
+      <div class="text-content">
+        <p>Sorry no scheduled tournaments at the moment.</p>
+      </div>
+    );
+  }
+  return (
+    <div class="text-content">
+      <h2>3rd Annual Riichi Mahjong Tournament</h2>
+      <p>
+It's that time of year again! We'll be having our Annual Club Tournament! It'll be in the same place as last year (Talison Row Apts Game Room, near mailboxes).
+<br/>
+All skill levels are welcome as long as they know how to play the game already. (Can't teach from scratch mid-tourney).
+<br/>
+We will have prizes including something particularly cool donated by our own @itsmat124!
+<br/>
+The tournament will be run a little more similar to NARMA/WRC for practice, but will still be as casual as the previous two.
+<br/>
+Food (almost certainly pizza) will be provided partway into the event. Spectators are also welcome.</p>
+      <a class="btn btn-primary" rel="noreferrer" target="_blank" href="https://forms.gle/Pz8VK4G6fLYPAa8J7">Register Here</a>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import "./index.css";
 import App from "./App";
@@ -8,6 +9,9 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Helmet>
+        <title>CHS Riichi Mahjong</title>
+      </Helmet>
       <App />
     </BrowserRouter>
   </React.StrictMode>
