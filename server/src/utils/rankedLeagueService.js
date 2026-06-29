b67847fb1579ec3bbf2c4f3c84507b81f884a121
cman131 <conor13@msn.com>
2026-06-29 13:45:46 -0400

    feat: add updateRankedPoints to rankedLeagueService

--- server/src/utils/rankedLeagueService.js
+++ server/src/utils/rankedLeagueService.js
@@ -1,6 +1,8 @@
 const RankedLeague = require('../models/RankedLeague');
 
 const SEASON_DURATION_DAYS = 90;
+const RANKED_STARTING_POINT = 30000;
+const RANK_UMA_BONUS = { 1: 30, 2: 10, 3: -10, 4: -30 };
 
 async function getCurrentLeague() {
   const latestLeague = await RankedLeague.findOne().sort({ startDate: -1 });
@@ -18,4 +20,22 @@ async function getCurrentLeague() {
   return latestLeague;
 }
 
-module.exports = { getCurrentLeague };
+async function updateRankedPoints(game) {
+  const league = await getCurrentLeague();
+
+  for (const gamePlayer of game.players) {
+    const playerId = gamePlayer.player.toString ? gamePlayer.player.toString() : String(gamePlayer.player);
+    const leaguePlayer = league.players.find(p => p.player.toString() === playerId);
+    if (!leaguePlayer) continue;
+
+    const umaBase = (Number(gamePlayer.score) - RANKED_STARTING_POINT) / 1000;
+    const rankBonus = RANK_UMA_BONUS[gamePlayer.rank] ?? 0;
+    leaguePlayer.rankedPoints += umaBase + rankBonus;
+    leaguePlayer.gamesPlayed += 1;
+  }
+
+  league.markModified('players');
+  await league.save();
+}
+
+module.exports = { getCurrentLeague, updateRankedPoints };
